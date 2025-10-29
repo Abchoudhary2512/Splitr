3f6a678f3521434e8fb8aa32b3c129269d5fea5e
Abchoudhary2512 <bhakarajay033@gmail.com>
2025-10-29 21:42:56 +0530

    feat: initialize the project

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>hii there
+      <Button variant="destructive">CLick me</Button>
+    </div>
+  );
+}
